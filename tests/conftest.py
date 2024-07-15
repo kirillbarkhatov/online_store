0bb4b18e36ac61c1eb2bee4be7ec36dd02fa25a7
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-15 19:49:30 +0300

    utils и тесты к нему

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,3 +59,8 @@ def category2(product4):
         description="Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
         products=[product4]
     )
+
+
+@pytest.fixture
+def products_by_category_list_of_dict():
+    return [{'name': 'Смартфоны', 'description': 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни', 'products': [{'name': 'Samsung Galaxy C23 Ultra', 'description': '256GB, Серый цвет, 200MP камера', 'price': 180000.0, 'quantity': 5}, {'name': 'Iphone 15', 'description': '512GB, Gray space', 'price': 210000.0, 'quantity': 8}, {'name': 'Xiaomi Redmi Note 11', 'description': '1024GB, Синий', 'price': 31000.0, 'quantity': 14}]}, {'name': 'Телевизоры', 'description': 'Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником', 'products': [{'name': '55" QLED 4K', 'description': 'Фоновая подсветка', 'price': 123000.0, 'quantity': 7}]}]
---
+++ tests/test_units.py
@@ -0,0 +1,18 @@
+from unittest.mock import mock_open, patch
+from src.utils import read_json, create_products_by_categories_from_dict
+
+
+def test_read_json():
+    m = mock_open(read_data="""{"test_key": "test_value"}""")
+    with patch("builtins.open", m):
+        assert read_json("test_path") == {"test_key": "test_value"}
+
+
+def test_create_products_by_categories_from_dict(products_by_category_list_of_dict):
+    categories_list = create_products_by_categories_from_dict(products_by_category_list_of_dict)
+    assert categories_list[0].name == "Смартфоны"
+    assert categories_list[1].description == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    assert categories_list[0].products[0].name == "Samsung Galaxy C23 Ultra"
+    assert categories_list[0].products[1].description == "512GB, Gray space"
+    assert categories_list[0].products[2].price == 31000.0
+    assert categories_list[1].products[0].quantity == 7
