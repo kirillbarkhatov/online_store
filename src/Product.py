b8efde86a6b4790b32d2503e58e86ebf1a4793ac
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-15 18:19:18 +0300

    Product class

---
+++ src/Product.py
@@ -0,0 +1,11 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
