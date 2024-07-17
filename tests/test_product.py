a4c721978d45984136c176fb32197d65760ed45b
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-17 21:23:16 +0300

    test_product.py 100%

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,40 @@
-def test_product(product1, product2, product3, product4):
+from unittest.mock import patch
+from src.product import Product
+
+def test_product(capsys, product1, product2, product3, product4):
     assert product1.name == "Samsung Galaxy S23 Ultra"
     assert product2.description == "512GB, Gray space"
     assert product3.price == 31000.0
     assert product4.quantity == 7
+
+    product3.price = 40000
+    assert product3.price == 40000
+    product3.price = 0
+    captured = capsys.readouterr()
+    assert captured.out == "Цена не должна быть нулевая или отрицательная\n"
+    assert product3.price == 40000
+
+    with patch("builtins.input") as mock_input:
+        mock_input.return_value = "y"
+        product3.price = 100
+        assert product3.price == 100
+
+    with patch("builtins.input") as mock_input:
+        mock_input.return_value = "n"
+        product3.price = 50
+        captured = capsys.readouterr()
+        assert captured.out == "Цена не была изменена\n"
+        assert product3.price == 100
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+         "quantity": 5}, )
+
+    assert new_product.name == "Samsung Galaxy S23 Ultra"
+
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 160000.0,
+         "quantity": 5}, [product1, product2, product3, product4])
+
+    assert new_product.quantity == 10
+    assert new_product.price == 180000.0
