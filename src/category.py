d05867972bfc49a2136f7d1cf4fb6ed79ff72ccd
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-17 21:04:31 +0300

    category обновлены методы

--- src/category.py
+++ src/category.py
@@ -1,13 +1,30 @@
+from src.product import Product
+
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+    @property
+    def products_list(self):
+        return self.__products
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
