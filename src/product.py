940dea7ed9d23457e0b02475547642fd752d3b10
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-22 20:07:17 +0300

    Product __add__ добавлен

--- src/product.py
+++ src/product.py
@@ -16,6 +16,9 @@ class Product:
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
+    def __add__(self, other):
+        return self.__price * self.quantity + other.__price * other.quantity
+
     @property
     def price(self) -> float:
         return self.__price
