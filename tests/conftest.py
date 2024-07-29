ddbfe0aa81fce28240f550da4ba56bcf568135ca
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-29 22:08:54 +0300

    Тесты смартфонов

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from src.category import Category
 from src.product import Product
+from src.smartphone import Smartphone
+from src.lawn_grass import LawnGrass
 from src.product_iterator import ProductIterator
 
 
@@ -76,3 +78,29 @@ def products_by_category_list_of_dict():
 @pytest.fixture
 def product_iterator(category1):
     return ProductIterator(category1)
+
+
+@pytest.fixture
+def smartphone1():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                         "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def smartphone2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+
+@pytest.fixture
+def smartphone3():
+    return Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+
+
+@pytest.fixture
+def grass1():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def grass2():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
---
+++ tests/test_smartphone.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_smartphone_init(smartphone1):
+    assert smartphone1.color == "Серый"
+
+
+def test_smartphone_add(smartphone2, smartphone3):
+    assert smartphone2 + smartphone3 == 2114000
+
+def test_smartphone_add_error(smartphone1, product2):
+    with pytest.raises(TypeError):
+        assert smartphone1 + product2
