c5b4454178f0e3a42dda0ebbefa9ae4927d7dd53
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-29 22:11:24 +0300

    Тесты газонов

---
+++ tests/test_lawn_grass.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def test_lawn_grass_init(grass1):
+    assert grass1.color == "Зеленый"
+
+
+def test_lawn_grass_add(grass1, grass2):
+    assert grass1 + grass2 == 16750
+
+def test_lawn_grass_add_error(grass1, product2):
+    with pytest.raises(TypeError):
+        assert grass1 + product2
