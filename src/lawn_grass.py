2049304a0b9aacc6f15e9d4d2ea5021b50cf7b91
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-29 21:36:25 +0300

    Добавлен класс LawnGrass

---
+++ src/lawn_grass.py
@@ -0,0 +1,19 @@
+from src.product import Product
+
+
+class LawnGrass(Product):
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(self, name: str,
+                 description: str,
+                 price: float,
+                 quantity: int,
+                 country: str,
+                 germination_period: str,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
