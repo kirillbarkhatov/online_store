f8d439a4c72612de0717da1b86af6709fc3c1877
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-08-05 22:39:51 +0300

    тесты Category

--- tests/conftest.py
+++ tests/conftest.py
@@ -105,3 +105,13 @@ def grass1():
 @pytest.fixture
 def grass2():
     return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
+
+@pytest.fixture
+def category_without_products():
+    return Category(
+        name="Смартфоны",
+        description="""Смартфоны, как средство не только коммуникации, 
+но и получения дополнительных функций для удобства жизни""",
+        products=[],
+    )
