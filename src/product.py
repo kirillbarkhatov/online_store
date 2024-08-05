b24c3a4674b79adfdfa1d71f472fd5942e6d776f
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-08-05 21:26:11 +0300

    Product - исключение для количества товара

--- src/product.py
+++ src/product.py
@@ -14,7 +14,10 @@ class Product(BaseProduct, PrintMixin):
         self.name = name
         self.description = description
         self.__price = price
-        self.quantity = quantity
+        if quantity > 0:
+            self.quantity = quantity
+        else:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         super().__init__()
 
     def __str__(self) -> str:
